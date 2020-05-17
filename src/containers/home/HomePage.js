857af506a43bf79598345ee9bb2325be7abe683e
Elliott Lovell <elliott@repositpower.com>
2020-05-17 12:22:08 +1000

    working search

--- src/containers/home/HomePage.js
+++ src/containers/home/HomePage.js
@@ -6,7 +6,8 @@ import * as professionalActions from "../../redux/actions/professional";
 import {bindActionCreators} from "redux/es/redux";
 
 const mapStateToProps = state => ({
-  professionals: state.professionals.active
+  professionals: state.professionals.active,
+  currentProfessional: state.professionals.currentProfessional
 });
 
 const mapDispatchToProps = dispatch => {
@@ -17,31 +18,93 @@ const mapDispatchToProps = dispatch => {
 
 
 class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "",
+      errorMessage: ""
+    };
+  }
+
   componentDidMount = () => {
-    console.log("HEllo1");
     this.props.professionalActions.fetchProfessionals();
-    console.log("HEllo2");
+  };
+
+  handleOnRowClick = (id) => {
+    this.props.professionalActions.fetchProfessional(id);
+  };
+
+  handleOnFavouriteClick = (id) => {
+    this.props.professionalActions.favouriteProfessional(id);
+  };
+
+  handleFieldOnChange = (field, value) => {
+    this.setState({[field]: value})
+  };
+  
+  filterProfessionals = () => {
+    let filteredProfessionals = this.props.professionals;
+    const searchTerm = this.state.searchTerm;
+
+    if (searchTerm && searchTerm.trim() !== "") {
+      let searchLower = searchTerm.toLowerCase();
+      filteredProfessionals = filteredProfessionals.filter(professional =>
+        (professional.first_name && professional.first_name.toLowerCase().indexOf(searchLower) >= 0) ||
+        (professional.last_name && professional.last_name.toLowerCase().indexOf(searchLower) >= 0) ||
+        (professional.description && professional.description.toLowerCase().indexOf(searchLower) >= 0) ||
+        (professional.notes && professional.notes.toLowerCase().indexOf(searchLower) >= 0)
+      );
+    }
+
+    filteredProfessionals.sort((a, b) => {
+      const aName = a.first_name;
+      const bName = b.last_name;
+      if (aName < bName) return -1;
+      if (aName > bName) return 1;
+      return 0;
+    });
+
+    return filteredProfessionals;
   };
 
   render() {
-    if (this.props.professionals) {
+    let professionals = this.props.professionals;
+    if (professionals) {
+      let filteredProfessionals = this.filterProfessionals();
       return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <div>{this.props.professionals[0].first_name} {this.props.professionals[0].last_name}</div>
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
+        <div className="homepage">
+          <header className="homepage-header"/>
+          <div className="professional-body">
+            <div className="search-container">
+              <span
+                onChange={(e) => this.handleFieldOnChange("searchTerm", e.target.value)}
+              >
+                <input
+                  className="search-input"
+                  placeholder="Search by name, specialty, or location ..."
+                />
+              </span>
+            </div>
+            <ul className="professional-list">
+            {filteredProfessionals.map((professional) => {
+              return (
+                <li className="professional-list-item" onClick={() => this.handleOnRowClick(professional.id)}>
+                  <div className="professional-info">
+                    <div className="professional-name">
+                      {professional.first_name} {professional.last_name}
+                    </div>
+                    <div className="professional-description">
+                      {professional.description}
+                    </div>
+                  </div>
+                  <div className="professional-favourite">
+                    <img src={logo} className="favourite" alt="logo"/>
+                  </div>
+                </li>
+              )
+            })}
+            </ul>
+          </div>
         </div>
       );
     } else {
--- src/redux/actions/professional.js
+++ src/redux/actions/professional.js
@@ -1,5 +1,8 @@
 export const FETCH_PROFESSIONALS = "FETCH_PROFESSIONALS";
 export const UPDATE_PROFESSIONALS = "UPDATE_PROFESSIONALS";
+export const FETCH_PROFESSIONAL = "FETCH_PROFESSIONAL";
+export const UPDATE_PROFESSIONAL = "UPDATE_PROFESSIONAL";
+export const FAVOURITE_PROFESSIONAL = "FAVOURITE_PROFESSIONAL";
 
 
 export function fetchProfessionals() {
@@ -21,3 +24,33 @@ function updateProfessionals(data) {
     }
   }
 }
+
+export function fetchProfessional() {
+  return {
+    type: FETCH_PROFESSIONAL,
+    meta: {
+      method: 'GET',
+      endpoint: "/api/professional/",
+      success: updateProfessional,
+    }
+  }
+}
+
+function updateProfessional(data) {
+  return {
+    type: UPDATE_PROFESSIONAL,
+    payload: {
+      currentProfessional: data.results
+    }
+  }
+}
+
+// function favouriteProfessional(data) {
+//   return {
+//     type: UPDATE_PROFESSIONALS,
+//     payload: {
+//       professionals: data.results
+//     },
+//     success:
+//   }
+// }
