e86ab2c7a42c245a33997778aec79525c2a81217
Elliott Lovell <elliott@repositpower.com>
2020-06-13 13:48:36 +1000

    Fixed abs value bug with kmToLatLng

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,7 @@
 
 export const kmToLatLng = (kilometers, latitude) => {
   let kmToLatRatio =  Math.PI / (180 * 110.574); // radians
-  let kmToLngRatio = Math.PI / (180 * 111.320 * Math.cos(latitude)); // radians
+  let kmToLngRatio = Math.abs(Math.PI / (180 * 111.320 * Math.cos(latitude))); // radians
 
   return [
     kilometers * kmToLatRatio * 180 / Math.PI, // latDelta
